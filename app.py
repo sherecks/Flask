c5c70d9b6432729efa39d314401a9fab4994cbf7
João Pedro Patrício do Nascimento <joaoppn00@gmail.com>
2022-09-24 11:29:20 -0300

    Teste de Expressões Jinja2

--- app.py
+++ app.py
@@ -6,14 +6,24 @@ app=Flask(__name__)
 
 @app.route("/joao")
 def hello():
+
+    #addition
+    idade1 = 21
+    idade2= 19
+
     return render_template(
-        "jinja_intro.html", name="João Pedro", template_name="Jinja2"
+        "jinja_intro.html", name="João Pedro", idade1="21", idade2="19"
         )
 
 
 
 @app.route("/pietra")
 def pietra():
+
+    #addition
+    idade3 = 19
+    idade4= 21
+
     return render_template(
-        "jinja2_intro.html", name="Pietra Pereria Nunes", template_name="Jinja2"
+        "jinja2_intro.html", name="Pietra Pereria Nunes", idade3="19", idade4="21"
         )
