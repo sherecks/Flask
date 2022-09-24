c2c4d5338ac29d4cdf422b54ef00814af79c07e2
João Pedro Patrício do Nascimento <joaoppn00@gmail.com>
2022-09-23 21:18:19 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return render_template("first.html")
+
+
+
+@app.route("/fancy")
+def fancy():
+    return render_template("second.html")
